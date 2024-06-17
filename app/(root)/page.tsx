5feb4601659a7e0b79754290f6befeefd1de2208
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-06-17 08:37:55 +0000

    refactor: Update Home page to use Navbar component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,24 +1,9 @@
-import { ThemeToggle } from "@/components/theme-toggle";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import Navbar from "@/components/navbar"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 w-full">
-      <div className="flex items-center justify-between flex-row-reverse w-full border border-white p-3 rounded-md bg-opacity-25 backdrop-blur-lg backdrop-filter">
-        <div className="flex gap-2 flex-row-reverse">
-          <Button className="border border-white p-4 rounded">Connect wallet</Button>
-          <ThemeToggle />
-        </div>
-        <div>
-          <Image
-            src="/images/next.svg"
-            alt="Next.js Logo"
-            width={72}
-            height={40}
-          />
-        </div>
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-between w-full overflow-hidden">
+      <Navbar />
     </main>
   );
 }
