19f846e3bc6d9fc2fa34ace1c7740603c2307fd8
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-06-17 07:08:33 +0000

    feat: Update Home page with ThemeToggle and Button components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,18 @@
+import { ThemeToggle } from "@/components/theme-toggle";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 w-full">
       <div className="flex items-center justify-between flex-row-reverse w-full border border-white p-3 rounded-md bg-opacity-25 backdrop-blur-lg backdrop-filter">
-        <button className="border border-white p-4 rounded">Connect wallet</button>
+        <div className="flex gap-2 flex-row-reverse">
+          <Button className="border border-white p-4 rounded">Connect wallet</Button>
+          <ThemeToggle />
+        </div>
         <div>
           <Image
-            src="./images/next.svg"
+            src="/images/next.svg"
             alt="Next.js Logo"
             width={72}
             height={40}
