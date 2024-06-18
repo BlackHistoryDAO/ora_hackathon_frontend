28ca54743949274995eecb04cec632e4e4f6c866
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-06-18 06:17:44 +0000

    refactor: Update Navbar component to include ConnectWallet and MetaMaskProvider

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,32 @@
+'use client';
+
 import Image from "next/image";
 import { ThemeToggle } from "./theme-toggle"
-import { Button } from "./ui/button"
 import Background from "./background"
+import ConnectWallect from "./connect";
+import { MetaMaskProvider } from "@metamask/sdk-react";
 
 const Navbar = () => {
+  const host =
+    typeof window !== "undefined" ? window.location.host : "defaultHost";
+
+  const sdkOptions = {
+    logging: { developerMode: false },
+    checkInstallationImmediately: false,
+    dappMetadata: {
+      name: "Next-Metamask-Boilerplate",
+      url: host,
+    },
+  };
   return (
     <Background image="images/bg-image.jpg">
       <div className="flex flex-col">
-        <div className="flex items-center justify-between flex-row-reverse border border-white p-3 rounded-md bg-gradient-to-tr from-white via-transparent to-black opacity-25 backdrop-blur-lg backdrop-filter m-8">
+        <div className="flex items-center justify-between flex-row-reverse border border-white p-3 rounded-md bg-opacity-25 backdrop-blur-lg backdrop-filter m-8">
           <div className="flex gap-2 flex-row-reverse">
-            <Button className="border border-white p-4 rounded">Connect wallet</Button>
+            <MetaMaskProvider debug={false} sdkOptions={sdkOptions}>
+              <ConnectWallect />
+            </MetaMaskProvider>
+ 
             <ThemeToggle />
           </div>
           <div>
@@ -22,7 +39,6 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-
     </Background>
 
   )
