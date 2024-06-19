03051015fa6d46e2067eb07a17962333d3e9aa3a
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-06-19 06:39:28 +0000

    refactor: Update ConnectContract component to use ethers.js for contract interaction

--- components/connectContract.tsx
+++ components/connectContract.tsx
@@ -1,6 +1,17 @@
+import { ethers } from "ethers";
+import { abi, contract_address } from "@/constants/abi";
+
 const ConnectContract = () => {
+
+  const connectContractDeployed =  async () => {
+    const provider = new ethers.AlchemyProvider('sepolia', process.env.SEPOLIA_ALCHEMY_API_KEY)
+    const connect = new ethers.Contract(contract_address, abi, provider)
+
+    const readContract = await connect.estimateFee(11);
+    return readContract.toString();
+  };
   return (
-    <div>ConnectContract</div>
+    <div className='container'>{connectContractDeployed()}</div>
   )
 }
 
